d1b753824d2816a2af815337fd629dee418b5295
andy-at-it <167446374+andy-at-it@users.noreply.github.com>
2024-05-02 15:12:09 +0100

    Add files via upload

--- email_subscribe.php
+++ email_subscribe.php
@@ -1,22 +1,14 @@
 <?php
 if (isset($_POST['submit'])) { 
-      $name = $_POST['name'];
-      $email = $_POST['email'];
-      $ToEmail = 'peekender.info@gmail.com';
-      $EmailSubject = 'Peek News Sunscribe request'; 
-      $mailheader = "From: ".$_POST["email"]."\r\n"; 
-      $mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
-      $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-      $MESSAGE_BODY = "Name: ".$_POST["name"]."<br>"; 
-      $MESSAGE_BODY .= "Email: ".$_POST["email"]."<br>"; 
-      if(mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader))
-      {
-		  echo "<script>alert('Subscribe EMail was sent !');</script>";
-		  echo "<script>document.location.href='email_subscribe.php'</script>";
-      }
-      else
-      {
-		echo "<script>alert('Mail was not sent. Please try again later');</script>";
-      }
- }
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$recipient = 'peekender.info@gmail.com';
+	$sender = $recipient;
+	$subject = 'Peek News Sunscribe request';
+	$body .= "Name: ".$_REQUEST['name']." \n";
+	$body .= "Email: ".$_REQUEST['email']." \n";	
+	$location = "connect_success.html";
+	mail( $recipient, $subject, $body, "From: $sender" ) or die ("Mail could not be sent.");	
+	header( "Location: $location" );
+}
 ?>
